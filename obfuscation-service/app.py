78bc3c6ef07e6180d3fb9f423488e9d4dbcaac21
Luca Alexandru UAIC <alexluca97@yahoo.com>
2021-12-13 03:07:26 +0200

    add labels and crop + paste obfuscated imgs

--- obfuscation-service/app.py
+++ obfuscation-service/app.py
@@ -6,7 +6,20 @@ import base64
 import random, sys
 app = Flask(__name__)
 
-
+labels = {
+  "photo": [42, 250, 780, 1130],
+  "serial": [1483, 300, 1613, 371],
+  "number": [1727, 300, 1980, 371], 
+  "cnp": [900, 378, 1483, 450],
+  "lastName": [789, 518, 1980, 600], 
+  "firstName": [789, 648, 1980, 730],
+  "nationality": [789, 778, 1980, 860], 
+  "placeOfBirth": [789, 908, 1980, 990],
+  "address": [789, 1038, 2250, 1215], 
+  "issuedBy": [789, 1260, 1483, 1350],
+  "valability": [1727, 1258, 2470, 1348],
+  "barCode": [100, 1380, 2430, 1680]
+}
 
 def isBase64(function):
   def wrapper(arg1):
@@ -73,10 +86,21 @@ def obfuscate():
     sb_bytes = pictureUrl
   
   decoded_image = base64.b64decode(sb_bytes)
+  img = Image.open(decoded_image).convert('RGB')
+  # left corner of the photo
+  xCI = 0
+  yCI = 0
+  labelKeys = {}
+  for key, value in labels.items():
+    crpImg = img.crop((xCI + value[0], yCI + value[1], xCI + value[2], yCI + value[3]))
+    obfuscateResult = obfuscateImage(crpImg)
+    img.paste(obfuscateResult[0], (xCI + value[0], yCI + value[1]))
+    labelKeys[key] = obfuscateResult[1]
+
   # go through dictionary of coordinates here and call obfuscateImage for every zone
   # paste the the encrypted zone back to the original photo
   # save the key in a object, associated with the label
-  return obfuscateImage(decoded_image)
+  return [labelKeys, img]
   
 @app.route('/deobfuscate', methods=['POST'])
 def deobfuscate():
